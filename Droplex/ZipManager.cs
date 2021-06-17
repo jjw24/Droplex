6a5356941d1ae848322ba1cb6c8ea0b5a232154d
Jeremy <jeremy24wu@gmail.com>
2021-06-17 13:16:48 +1000

    add capability for handling zip packages

---
+++ Droplex/ZipManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+
+namespace Droplex
+{
+    public static class ZipManager
+    {
+        public static bool IsZip(string filePath)
+        {
+            return string.Equals(Path.GetExtension(filePath), ".zip", StringComparison.OrdinalIgnoreCase);
+        }
+
+        public static void Extract(string sourceZipFilePath, string destinationExtractedPath)
+        {
+            ZipFile.ExtractToDirectory(sourceZipFilePath, destinationExtractedPath);
+        }
+    }
+}
