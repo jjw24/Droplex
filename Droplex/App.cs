e5521d598b0de0a9e907d5cd2b90798a01960f7a
Jeremy <jeremy24wu@gmail.com>
2021-07-22 21:38:44 +1000

    add Everything 1.4.1.1009 separately and fix putty 0.75

--- Droplex/App.cs
+++ Droplex/App.cs
@@ -8,9 +8,11 @@ namespace Droplex
     {
         python3_9_1 = 1,
         python3_8_7 = 2,
-        Everything1_4_1_1009 = 3,
+        Everything1_3_4_686 = 3,
         Putty_0_74 = 4,
         Zabbix_5_2_6 = 5,
-        python_3_8_9_embeddable = 6
+        python_3_8_9_embeddable = 6,
+        Putty_0_75 = 7,
+        Everything1_4_1_1009 = 8
     }
 }
