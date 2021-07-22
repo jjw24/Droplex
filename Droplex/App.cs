da7a7e5a773cfb72d2de77e919352f63c9ccaa3b
Kevin Zhang <45326534+taooceros@users.noreply.github.com>
2021-07-22 12:11:41 +0800

    Update App.cs

--- Droplex/App.cs
+++ Droplex/App.cs
@@ -8,7 +8,7 @@ namespace Droplex
     {
         python3_9_1 = 1,
         python3_8_7 = 2,
-        Everything1_3_4_686 = 3,
+        Everything1_4_1_1009 = 3,
         Putty_0_74 = 4,
         Zabbix_5_2_6 = 5,
         python_3_8_9_embeddable = 6
